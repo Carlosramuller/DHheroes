ec5eb279559095e4cf47dbcad4c7c34743be0ae3
Carlosramuller <carlosramuller@hotmail.com>
2021-06-10 17:56:23 -0300

    se completan links con imagenes e info, listo para entregar

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3030;
 const path = require('path');
 const views = path.join(__dirname, 'views');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(views, 'index.html')));
 app.get('/babbage', (req, res) => res.sendFile(path.join(views, 'babbage.html')));
